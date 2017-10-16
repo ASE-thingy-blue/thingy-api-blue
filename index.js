e1f0053b8b9237f942829319fe212248f3ab34d5
Salim Hermidas <salim@hermidas.ch>
2017-10-16 22:07:25 +0200

    TER-10: Initial Commit hapi Hello World

---
+++ index.js
@@ -0,0 +1,51 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Joi = require('joi');
+
+const server = new Hapi.Server();
+server.connection({
+    host: '127.0.0.1',
+    port: 8080,
+    routes: {cors: true}
+});
+
+const swaggerOptions = {
+    info: {
+        'title': 'thingy-api-blue',
+        'version': '1.0.0',
+        'description': 'thingy-api-blue'
+    }
+}
+
+server.register([
+    Inert,
+    Vision,
+    {
+        register: HapiSwagger,
+        options: swaggerOptions
+    }
+]);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply("Hello World!").code(200);
+    },
+    config: {
+        tags: ['api'],
+        description: 'List all todos',
+        plugins: {'hapi-swagger': {responses: {
+            200: {
+                description: 'Success',
+                schema: Joi.string
+            }
+        }}}
+    }
+});
+
+server.start((err) => {
+    console.log('Server running at:', server.info.uri);
+});
