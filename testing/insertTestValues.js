da14131fec878adc2b3353fa478013ba0925bd24
Lars Vögtlin <lvoegtlin@gmail.com>
2017-10-29 12:40:56 +0100

    you can now insert te values without calling it in the index file.

--- testing/insertTestValues.js
+++ testing/insertTestValues.js
@@ -1,3 +1,8 @@
+/*
+ * This files adds dummy entries into the db.
+ * start it with node {path/to/the/file}. e.g. node testing/insertTestValues.js
+ */
+
 const Mongoose = require('mongoose');
 
 var Terri = require("../model/Terrarium");
@@ -11,49 +16,48 @@ var User = require("../model/user");
 
 var Unit = require("../model/Unit");
 
-module.exports = function () {
+require("../model/helper/databaseConnection");
 
 //creating units
-    var celsius = new Unit({name: "Celsius", short: "C"});
-    var percent = new Unit({name: "Percent", short: "%"});
-    var gPerQ = new Unit({name: "gram per qubic meter", short: "g/m3"});
+var celsius = new Unit({name: "Celsius", short: "C"});
+var percent = new Unit({name: "Percent", short: "%"});
+var gPerQ = new Unit({name: "gram per qubic meter", short: "g/m3"});
 
-    celsius.save();
-    percent.save();
-    gPerQ.save();
+celsius.save();
+percent.save();
+gPerQ.save();
 
 //temp
-    var temp = new Temp({value: 25, unit: celsius});
-    temp.save();
+var temp = new Temp({value: 25, unit: celsius});
+temp.save();
 
 //hum
-    var hum = new Hum({value: 15, unit: percent});
-    hum.save();
+var hum = new Hum({value: 15, unit: percent});
+hum.save();
 
 //co2
-    var co2 = new Carbon({value: 2, unit: gPerQ});
-    co2.save();
+var co2 = new Carbon({value: 2, unit: gPerQ});
+co2.save();
 
 //airq
-    var airQ = new AirQ({co2: co2});
-    airQ.save();
+var airQ = new AirQ({co2: co2});
+airQ.save();
 
 //measure
-    var measure = new Measurement({airQuality: airQ, temperature: temp, humidity: hum});
-    measure.save();
+var measure = new Measurement({airQuality: airQ, temperature: temp, humidity: hum});
+measure.save();
 
 //thingy
-    var thingy = new Thingy({macAddress: "123", description: "test thingy"});
-    thingy.measurements.push(measure);
-    thingy.save();
+var thingy = new Thingy({macAddress: "123", description: "test thingy"});
+thingy.measurements.push(measure);
+thingy.save();
 
 //terri
-    var terri = new Terri({name: "test terri"});
-    terri.thingies.push(thingy);
-    terri.save();
+var terri = new Terri({name: "test terri"});
+terri.thingies.push(thingy);
+terri.save();
 
 //user
-    var user = new User({name: "Joe Slowinski", mailAddress: "schlangenman@gmail.com"});
-    user.terrariums.push(terri);
-    user.save();
-};
+var user = new User({name: "Joe Slowinski", mailAddress: "schlangenman@gmail.com"});
+user.terrariums.push(terri);
+user.save();
