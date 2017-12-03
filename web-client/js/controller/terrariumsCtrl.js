6eb05047733f49029e20a31ec03a09efdf37bf0d
Salim Hermidas <salim@hermidas.ch>
2017-12-03 20:48:59 +0100

    TER-36: Working on Terrarium View

--- web-client/js/controller/terrariumsCtrl.js
+++ web-client/js/controller/terrariumsCtrl.js
@@ -5,10 +5,40 @@
  */
 termonWebClient.controller('terrariumsCtrl', ['$scope', 'dataService', function($scope, dataService) {
 
-    $scope.message = 'Hello dudes!';
+    $scope.terrariums = [];
 
+    //view flags
+    $scope.terDetails = null;
+    $scope.thingyDetails = null;
+
+    console.log('init');
     dataService.get('/terrariums').then(function(data) {
-        console.log(data);
+        $scope.terrariums = data.terrariums;
+        angular.forEach($scope.terrariums, function(terrarium) {
+            dataService.get('/terrarium/'+terrarium._id+'/thingies').then(function(data) {
+                terrarium.thingies = data.thingies;
+            });
+        })
     });
+
+    $scope.hideDetails = function() {
+        $scope.terDetails = null;
+        $scope.thingyDetails = null;
+    };
+
+    $scope.showTerDetails = function(ter) {
+        dataService.get('/terrarium/'+ter._id+'/values').then(function(data) {
+            $scope.terDetails = data;
+            console.log($scope.terDetails);
+        });
+    };
+
+    $scope.showThingyDetails = function(ter, thingy) {
+        dataService.get('/terrarium/'+ter._id+'/thingies/'+thingy._id+'/values').then(function(data) {
+            $scope.thingyDetails = data;
+            console.log($scope.thingyDetails);
+        });
+    };
+
 
 }]);
