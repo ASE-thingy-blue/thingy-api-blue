33b7959580aff5d4699c4aaecc90dd1814986cf2
Salim Hermidas <salim@hermidas.ch>
2017-12-04 15:53:51 +0100

    Dont verify DB Certificate

--- model/helper/databaseConnection.js
+++ model/helper/databaseConnection.js
@@ -26,7 +26,7 @@ var create = function()
 		reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
 		reconnectInterval: 500, // Reconnect every 500ms
 		sslValidate: true,
-		checkServerIdentity: true,
+		checkServerIdentity: false,
 		sslCA: [ fs.readFileSync('certs/ThingyRootCA.cer') ],
 		sslCert: fs.readFileSync('certs/ThingyAPI.cer'),
 		sslKey: fs.readFileSync('certs/ThingyAPI.epk')
