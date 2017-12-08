8a6d6a081e30f1b007e492577d074eb0ef092247
icefractal <icefractal@github.com>
2017-12-08 18:38:01 +0100

    Consistent indentation

--- model/helper/databaseConnection.js
+++ model/helper/databaseConnection.js
@@ -5,118 +5,118 @@ const Path = require('path');
 
 var create = function()
 {
-	var dbUrl;
+    var dbUrl;
 
-	// Check if the process is running inside a Docker container
-	if (isDocker())
-	{
-		console.log('Running inside a Docker container');
-		dbUrl = 'mongodb://mongo:27017/thingy-api-blue?ssl=true';
-	}
-	else
-	{
-		console.log('NOT running inside a Docker container');
-		dbUrl = 'mongodb://127.0.0.1:27017/thingy-api-blue?ssl=true';
-	}
+    // Check if the process is running inside a Docker container
+    if (isDocker())
+    {
+        console.log('Running inside a Docker container');
+        dbUrl = 'mongodb://mongo:27017/thingy-api-blue?ssl=true';
+    }
+    else
+    {
+        console.log('NOT running inside a Docker container');
+        dbUrl = 'mongodb://127.0.0.1:27017/thingy-api-blue?ssl=true';
+    }
 
-	console.log("DB URL used: " + dbUrl);
+    console.log("DB URL used: " + dbUrl);
 
-	var mongoOptions =
-	{
-		useMongoClient: true,
-		reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-		reconnectInterval: 500, // Reconnect every 500ms
-		sslValidate: true,
-		checkServerIdentity: true,
-		sslCA: [ Fs.readFileSync(Path.join('certs', 'ThingyRootCA.cer')) ],
-		sslCert: Fs.readFileSync(Path.join('certs', 'ThingyAPI.cer')),
-		sslKey: Fs.readFileSync(Path.join('certs', 'ThingyAPI.epk'))
-	};
+    var mongoOptions =
+    {
+        useMongoClient: true,
+        reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+        reconnectInterval: 500, // Reconnect every 500ms
+        sslValidate: true,
+        checkServerIdentity: true,
+        sslCA: [ Fs.readFileSync(Path.join('certs', 'ThingyRootCA.cer')) ],
+        sslCert: Fs.readFileSync(Path.join('certs', 'ThingyAPI.cer')),
+        sslKey: Fs.readFileSync(Path.join('certs', 'ThingyAPI.epk'))
+    };
 
-	// The wait loop for the DB connection
-	function createConnection(dbUrl, mongoOptions)
-	{
-		var db = Mongoose.connect(dbUrl, mongoOptions);
+    // The wait loop for the DB connection
+    function createConnection(dbUrl, mongoOptions)
+    {
+        var db = Mongoose.connect(dbUrl, mongoOptions);
 
-		db.on('error', function(err)
-		{
-			// If first connect fails because mongod is down, try again later.
-			// This is only needed for first connect, not for runtime reconnects.
-			// See: https://github.com/Automattic/mongoose/issues/5169
-			if (err.message && err.message.match(/failed to connect to server .* on first connect/))
-			{
-				console.error(new Date(), String(err));
+        db.on('error', function(err)
+        {
+            // If first connect fails because mongod is down, try again later.
+            // This is only needed for first connect, not for runtime reconnects.
+            // See: https://github.com/Automattic/mongoose/issues/5169
+            if (err.message && err.message.match(/failed to connect to server .* on first connect/))
+            {
+                console.error(new Date(), String(err));
 
-				// Wait for a bit, then try to connect again
-				setTimeout(function()
-				{
-					console.log("Retrying first connect ...");
-					db.openUri(dbUrl).catch(() => {});
-					// Why the empty catch?
-					// Well, errors thrown by db.open() will also be passed to .on('error'),
-					// so we can handle them there, no need to log anything in the catch here.
-					// But we still need this empty catch to avoid unhandled rejections.
-				}, 20000);
-			}
-			else
-			{
-				// Some other error occurred. Log it.
-				console.error(new Date(), String(err));
-			}
-		});
+                // Wait for a bit, then try to connect again
+                setTimeout(function()
+                {
+                    console.log("Retrying first connect ...");
+                    db.openUri(dbUrl).catch(() => {});
+                    // Why the empty catch?
+                    // Well, errors thrown by db.open() will also be passed to .on('error'),
+                    // so we can handle them there, no need to log anything in the catch here.
+                    // But we still need this empty catch to avoid unhandled rejections.
+                }, 20000);
+            }
+            else
+            {
+                // Some other error occurred. Log it.
+                console.error(new Date(), String(err));
+            }
+        });
 
-		db.once('open', function()
-		{
-			console.log("Connection to DB established.");
-		});
-	}
+        db.once('open', function()
+        {
+            console.log("Connection to DB established.");
+        });
+    }
 
-	createConnection(dbUrl, mongoOptions);
+    createConnection(dbUrl, mongoOptions);
 
-	// The DB connection
-	var db = Mongoose.connection;
+    // The DB connection
+    var db = Mongoose.connection;
 
-	db.on('connected', function()
-	{
-		console.log('MongoDB connected!');
-	});
+    db.on('connected', function()
+    {
+        console.log('MongoDB connected!');
+    });
 
-	db.once('open', function()
-	{
-		console.log('MongoDB connection opened!');
-	});
+    db.once('open', function()
+    {
+        console.log('MongoDB connection opened!');
+    });
 
-	// Hook if the DB loses connection (mongo container is not reachable)
-	db.on('disconnected', function()
-	{
-		console.log('MongoDB disconnected!');
-	});
+    // Hook if the DB loses connection (mongo container is not reachable)
+    db.on('disconnected', function()
+    {
+        console.log('MongoDB disconnected!');
+    });
 
-	// Hook if we reconnect to the DB server
-	db.on('reconnected', function()
-	{
-		console.log('MongoDB reconnected!');
-	});
+    // Hook if we reconnect to the DB server
+    db.on('reconnected', function()
+    {
+        console.log('MongoDB reconnected!');
+    });
 
-	// If the Node process ends, close the Mongoose connection
-	db.on('SIGINT', function()
-	{
-		mongoose.connection.close(function()
-		{
-			console.warn('Mongoose disconnected on app interruption');
-			process.exit(0);
-		});
-	});
+    // If the Node process ends, close the Mongoose connection
+    db.on('SIGINT', function()
+    {
+        mongoose.connection.close(function()
+        {
+            console.warn('Mongoose disconnected on app interruption');
+            process.exit(0);
+        });
+    });
 
-	// If the Node process ends, close the Mongoose connection
-	db.on('SIGTERM', function()
-	{
-		mongoose.connection.close(function()
-		{
-			console.warn('Mongoose disconnected on app termination');
-			process.exit(0);
-		});
-	});
+    // If the Node process ends, close the Mongoose connection
+    db.on('SIGTERM', function()
+    {
+        mongoose.connection.close(function()
+        {
+            console.warn('Mongoose disconnected on app termination');
+            process.exit(0);
+        });
+    });
 };
 
 module.exports = create();
--- model/schemas/targetValues.js
+++ model/schemas/targetValues.js
@@ -7,33 +7,33 @@ const Mongoose = require('mongoose');
 var targetValuesSchema = Mongoose.Schema({
 
     temperature : {
-	value : {
-	    type : Number,
-	    require : true
-	},
-	unit : require('./unit')
+    value : {
+        type : Number,
+        require : true
+    },
+    unit : require('./unit')
     },
 
     humidity : {
-	value : {
-	    type : Number,
-	    require : true
-	},
-	unit : require('./unit')
+    value : {
+        type : Number,
+        require : true
+    },
+    unit : require('./unit')
     },
     co2 : {
-	value : {
-	    type : Number,
-	    require : true
-	},
-	unit : require('./unit')
+    value : {
+        type : Number,
+        require : true
+    },
+    unit : require('./unit')
     },
     tvoc : {
-	value : {
-	    type : Number,
-	    require : true
-	},
-	unit : require('./unit')
+    value : {
+        type : Number,
+        require : true
+    },
+    unit : require('./unit')
     },
 
 });
