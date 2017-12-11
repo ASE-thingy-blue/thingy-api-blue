6ba859226a567ecf485db57d4c4cd0b51caa42cf
Salim Hermidas <salim@hermidas.ch>
2017-12-11 17:42:31 +0100

    TER-36: Show current Terrarium/Thingy Values

--- web-client/js/controller/terrariumsCtrl.js
+++ web-client/js/controller/terrariumsCtrl.js
@@ -11,22 +11,29 @@ termonWebClient.controller('terrariumsCtrl', ['$scope', 'dataService', function(
     $scope.terDetails = null;
     $scope.thingyDetails = null;
 
-    console.log('init');
-    dataService.get('/terrariums').then(function(data) {
-        $scope.terrariums = data.terrariums;
-        angular.forEach($scope.terrariums, function(terrarium) {
-            dataService.get('/terrarium/'+terrarium._id+'/thingies').then(function(data) {
-                terrarium.thingies = data.thingies;
-            });
-        })
-    });
-
     $scope.hideDetails = function() {
         $scope.terDetails = null;
         $scope.thingyDetails = null;
     };
 
+    console.log('init');
+    $scope.loadData = function() {
+        $scope.hideDetails();
+
+        dataService.get('/terrariums').then(function(data) {
+            $scope.terrariums = data.terrariums;
+            angular.forEach($scope.terrariums, function(terrarium) {
+                dataService.get('/terrarium/'+terrarium._id+'/thingies').then(function(data) {
+                    terrarium.thingies = data.thingies;
+                });
+            })
+        });
+    };
+    $scope.loadData();
+
     $scope.showTerDetails = function(ter) {
+        $scope.hideDetails();
+
         dataService.get('/terrarium/'+ter._id+'/values').then(function(data) {
             $scope.terDetails = data;
             console.log($scope.terDetails);
@@ -34,6 +41,8 @@ termonWebClient.controller('terrariumsCtrl', ['$scope', 'dataService', function(
     };
 
     $scope.showThingyDetails = function(ter, thingy) {
+        $scope.hideDetails();
+        
         dataService.get('/terrarium/'+ter._id+'/thingies/'+thingy._id+'/values').then(function(data) {
             $scope.thingyDetails = data;
             console.log($scope.thingyDetails);
